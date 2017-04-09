b7bd6a0694c0fe699e1cc94254937c4cad2e6f5a
Hien <hien@Hiens-Mac.local>
2017-04-09 15:42:56 +0700

    Initial Commit

---
+++ Giai_PT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Giai_PT
+//
+//  Created by Hien on 4/9/17.
+//  Copyright © 2017 Hien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
